30321588b9fd92015851898e187fdc173bb835df
Chao Wang <wangchao@xyz10.com>
2021-01-04 11:16:45 +0800

    feat: p509 update

--- src/p509_fib.rs
+++ src/p509_fib.rs
@@ -10,18 +10,20 @@ pub struct Solution {}
 impl Solution {
     pub fn p509_fib(n: i32) -> i32 {
         // codes
-        let n = n as usize;
-        let mut dp = vec![0; (n + 1).max(3)];
-
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-
-        for i in 3..=n {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        if n < 2 {
+            return n;
         }
 
-        dp[n]
+        let mut prepre = 0;
+        let mut pre = 1;
+
+        for _ in 3..=n {
+            let cur = pre + prepre;
+            prepre = pre;
+            pre = cur;
+        }
+
+        pre + prepre
     }
 }
 
